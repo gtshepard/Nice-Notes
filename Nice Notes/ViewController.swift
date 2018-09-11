6e5ca6ab28bde42a2affa4f0c61d45496254ed7f
Garrison Shepard <shepard.garrisont@gmail.com>
2018-09-11 15:01:42 -0400

    Initial Commit

---
+++ Nice Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nice Notes
+//
+//  Created by Garrison Shepard on 9/11/18.
+//  Copyright © 2018 Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
