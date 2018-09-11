d934d996e9b26de7e488c499773f9a5d2a098427
Garrison Shepard <shepard.garrisont@gmail.com>
2018-09-11 16:04:08 -0400

    feat: implement UI Basics
    set up both view controllers, added assets, and set up tabelview class

---
+++ Nice Notes/NoteViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NoteViewController.swift
+//  Nice Notes
+//
+//  Created by Garrison Shepard on 9/11/18.
+//  Copyright © 2018 Garrison. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+}
--- Nice Notes/ViewController.swift
+++ Nice Notes/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var addNoteButton: UIBarButtonItem!
+    @IBOutlet weak var notesTableView: UITableView!
+    
+    //collection of notes
+    var notes: [NSManagedObject] = []
+    var imagesForNoteCells: [UIImage] = [#imageLiteral(resourceName: "bunny"),#imageLiteral(resourceName: "character"),#imageLiteral(resourceName: "parrot"),#imageLiteral(resourceName: "snail"),#imageLiteral(resourceName: "squirrel"), #imageLiteral(resourceName: "wind")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        notesTableView.delegate = self
+    }
+    
+    @IBAction func addNote(_ sender: Any) {
+        createNote()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func selectImageForCell() -> UIImage{
+        let index = Int(arc4random_uniform(UInt32(imagesForNoteCells.count)))
+        return imagesForNoteCells[index]
     }
-
-
+    func createNote(){
+        
+        let alert = UIAlertController(title: "Name Your Note", message: "Name Your Voice Note", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Add", style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            print("NAME: \(nameToSave)")
+          //  self.fileName = nameToSave
+           // self.configureUI(button: self.recordButton, value: true)
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",style: .default)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let noteCell = tableView.dequeueReusableCell(withIdentifier: "noteCell")!
+        
+        return noteCell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
