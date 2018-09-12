9b95bb832c4b8601946785590035deffc4ee32d9
Garrison Shepard <shepard.garrisont@gmail.com>
2018-09-12 02:49:35 -0400

    feat: notes can be written, but not saved

--- Nice Notes/NoteViewController.swift
+++ Nice Notes/NoteViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class NoteViewController: UIViewController {
 
+ 
+    @IBOutlet weak var notebookButton: UIBarButtonItem!
+    @IBOutlet weak var noteNameTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var noteTextField: UITextView!
+    
+    var noteName:String!
+    var noteText:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    @IBAction func backToNotebook(_ sender: Any) {
+        
+      // _ = self.navigationController?.popViewController(animated: true)
+     
+        
+        
+    }
     
+    
+    @IBAction func saveNote(_ sender: Any) {
+        
+        //pass data back to previous vc
+        //then use save method
+        
+    }
     
     
     
--- Nice Notes/ViewController.swift
+++ Nice Notes/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var notesTableView: UITableView!
     
     //collection of notes
-    var names: [String] = []
+    var notebook: [NSManagedObject] = []
     var imagesForNoteCells: [UIImage] = [#imageLiteral(resourceName: "bunny"),#imageLiteral(resourceName: "character"),#imageLiteral(resourceName: "parrot"),#imageLiteral(resourceName: "snail"),#imageLiteral(resourceName: "squirrel"), #imageLiteral(resourceName: "wind")]
     
     override func viewDidLoad() {
@@ -25,50 +25,108 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         notesTableView.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Note")
+        
+        //3
+        do {
+            notebook = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
     
     @IBAction func addNote(_ sender: Any) {
      
-        let alert = UIAlertController(title: "New Name",
-                                      message: "Add a new name",
-                                      preferredStyle: .alert)
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
         
-        let saveAction = UIAlertAction(title: "Save",
-                                       style: .default) {
-                                        [unowned self] action in
-                                        
-                                        guard let textField = alert.textFields?.first,
-                                            let nameToSave = textField.text else {
-                                                return
-                                        }
-                                        
-                                        self.names.append(nameToSave)
-                                        self.notesTableView.reloadData()
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text else {
+                return
+            }
+            self.save(name: nameToSave, text: "")
+            self.notesTableView.reloadData()
         }
-        
-        let cancelAction = UIAlertAction(title: "Cancel",
-                                         style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
         
         alert.addTextField()
-        
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
         
         present(alert, animated: true)
     }
-
+    
+    
+    func save(name: String, text: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity = NSEntityDescription.entity(forEntityName: "Note", in: managedContext)!
+        
+        let note = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // 3
+       note.setValue(name, forKeyPath: "name")
+       note.setValue(text, forKeyPath: "text")
+        
+        // 4
+        do {
+            try managedContext.save()
+            notebook.append(note)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showNote"{
+            let noteViewController  = segue.destination as! NoteViewController
+            let noteName = sender as! String
+            noteViewController.noteName = noteName
+        }
+    }
+    
     func selectImageForCell() -> UIImage{
         let index = Int(arc4random_uniform(UInt32(imagesForNoteCells.count)))
         return imagesForNoteCells[index]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return names.count
+            return notebook.count
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath)
-                cell.textLabel?.text = names[indexPath.row]
-                return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath)
+        let note = notebook[indexPath.row]
+        cell.textLabel?.text = note.value(forKeyPath: "name") as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let note = notebook[indexPath.row]
+        let noteName = note.value(forKey: "name")
+        performSegue(withIdentifier: "showNote", sender: noteName)
     }
     
 }
